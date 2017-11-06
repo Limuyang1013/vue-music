a1f20ee65fedef74dc204662b4acb52f39c49fe5
Limuyang <lin794653318@gmail.com>
2017-11-06 11:04:59 +0800

    项目初始化:moon:

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable.styl"
+  #app
+    color $color-theme
+</style>
