3c2a2c29fd0fe76d1b83e3d661adff4126eed489
Limuyang <lin794653318@gmail.com>
2017-11-15 17:50:17 +0800

    歌手列表字母组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
             <ul>
               <li v-for="item in discList" class="item">
                 <div class="icon">
-                  <img width="60" height="60px" :src="item.imgurl"/>
+                  <img width="60" height="60px" v-lazy="item.imgurl"/>
                 </div>
                 <div class="text">
                   <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,11 +26,15 @@
             </ul>
           </div>
         </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
       </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Loading from '../../base/loading/loading'
   import Scroll from '../../base/scroll/scroll'
   import Slider from '../../base/slider/slider'
   import {getRecommend, getDiscList} from '../../api/recommend'
@@ -70,7 +74,8 @@
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
 import './common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
 
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
