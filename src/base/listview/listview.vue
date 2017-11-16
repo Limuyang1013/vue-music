290cfd949ee9bcccb9494dd9d22a53657de80d74
Limuyang <lin794653318@gmail.com>
2017-11-16 15:04:00 +0800

    歌手列表页面开发完毕

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,5 +1,5 @@
 <template>
-    <scroll class="listview" :data="data" ref="listview">
+    <scroll class="listview" :data="data" ref="listview" :listenScroll="listenScroll" :probeType="probeType" @scroll="scroll">
       <ul>
         <li v-for="group in data" class="list-group" ref="listGroup">
           <h2 class="list-group-title">{{group.title}}</h2>
@@ -13,21 +13,40 @@
       </ul>
       <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
         <ul>
-          <li v-for="(item, index) in shortCutList" class="item" :data-index="index">
+          <li v-for="(item, index) in shortCutList" class="item" :data-index="index" :class="{'current': currentIndex === index}">
             {{item}}
           </li>
         </ul>
+      </div>
+      <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+        <h1 class="fixed-title">{{fixedTitle}}</h1>
+      </div>
+      <div v-show="!data.length" class="loading-container">
+        <loading></loading>
       </div>
     </scroll>
 </template>
 
 <script type="text/ecmascript-6">
-  import Scroll from '../../base/scroll/scroll.vue'
+  import Scroll from '../../base/scroll/scroll'
+  import Loading from '../../base/loading/loading'
   import {getData} from '../../common/js/dom'
+
   const ANCHOR_HEIGHT = 18
+  const TITLE_HEIGHT = 30
   export default {
     created() {
       this.touch = {}
+      this.listenScroll = true
+      this.listHieght = []
+      this.probeType = 3
+    },
+    data() {
+      return {
+        scrollY: -1,
+        currentIndex: 0,
+        diff: -1
+      }
     },
     props: {
       data: {
@@ -40,6 +59,12 @@
         return this.data.map((group) => {
           return group.title.substr(0, 1)
         })
+      },
+      fixedTitle() {
+        if (this.scrollY > 0) {
+          return ''
+        }
+        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
       }
     },
     methods: {
@@ -57,12 +82,71 @@
         let anchorIndex = parseInt(this.touch.anchorIndex) + delta
         this._scrollTo(anchorIndex)
       },
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
       _scrollTo(index) {
+        if (!index && index !== 0) {
+          return
+        }
+        if (index < 0) {
+          index = 0
+        } else if (index > this.listHeight.length - 2) {
+          index = this.listHeight.length - 2
+        }
+        this.scrollY = -this.listHeight[index]
         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      },
+      _calculateHeight() {
+        this.listHeight = []
+        const list = this.$refs.listGroup
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this._calculateHeight()
+        }, 20)
+      },
+      scrollY(newY) {
+        const listHeight = this.listHeight
+        // 当滚动到顶部，newY > 0
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        // 在中间部分滚动
+        for (let i = 0; i < listHeight.length - 1; i++) {
+          let lowerLimit = listHeight[i]
+          let upperLimit = listHeight[i + 1]
+          if (!upperLimit || (-newY >= lowerLimit && -newY < upperLimit)) {
+            this.currentIndex = i
+            this.diff = upperLimit + newY
+            return
+          }
+        }
+        // 滚动到底部，且-newY大于最后一个元素的上限
+        this.currentIndex = this.listHeight.length - 2
+      },
+      diff(newVal) {
+        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+        if (this.fixedTop === fixedTop) {
+          return
+        }
+        this.fixedTop = fixedTop
+        this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
       }
     },
     components: {
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
