7ddd6ddcb1c21bf84950c7dc3e13d79ac3013c51
Limuyang <lin794653318@gmail.com>
2017-11-17 16:52:42 +0800

    歌手详情页

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -32,11 +32,12 @@
         }
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
           }
         })
       },
-      normalizeSongs(list) {
+      _normalizeSongs(list) {
         let ret = []
         list.forEach((item) => {
           let {musicData} = item
