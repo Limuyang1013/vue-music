8922ec462547bf4fcf5390c153fe84d7223c612e
Limuyang <lin794653318@gmail.com>
2017-11-20 18:10:25 +0800

    修复上下滑动的部分问题

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -6,7 +6,7 @@
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" v-show="songs.length>0" ref="playBtn">
+        <div ref="playBtn" v-show="songs.length>0" class="play" @click="random">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -14,11 +14,12 @@
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
-    <scroll :data="songs" @scroll="scroll" :probe-Type="probeType" :listen-scroll="listenScroll" class="list" ref="list">
+    <scroll :data="songs" @scroll="scroll"
+            :listen-scroll="listenScroll" :probe-type="probeType" class="list" ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list :songs="songs" :rank="rank" @select="selectItem"></song-list>
       </div>
-      <div class="loading-container" v-show="!songs.length">
+      <div v-show="!songs.length" class="loading-container">
         <loading></loading>
       </div>
     </scroll>
@@ -26,10 +27,10 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import Scroll from '../../base/scroll/scroll'
-  import SongList from '../../base/song-list/song-list'
-  import {prefixStyle} from '../../common/js/dom'
-  import Loading from '../../base/loading/loading'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -48,6 +49,10 @@
       title: {
         type: String,
         default: ''
+      },
+      rank: {
+        type: Boolean,
+        default: false
       }
     },
     data() {
@@ -66,7 +71,7 @@
     },
     mounted() {
       this.imageHeight = this.$refs.bgImage.clientHeight
-      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+      this.minTransalteY = -this.imageHeight + RESERVED_HEIGHT
       this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
     methods: {
@@ -75,25 +80,36 @@
       },
       back() {
         this.$router.back()
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      random() {
+        this.randomPlay({
+          list: this.songs
+        })
       }
     },
     watch: {
-      scrollY(newY) {
-        let translateY = Math.max(this.minTranslateY, newY)
-        let zIndex = 0
+      scrollY(newVal) {
+        let translateY = Math.max(this.minTransalteY, newVal)
         let scale = 1
+        let zIndex = 0
         let blur = 0
-        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
-        const percent = Math.abs(newY / this.imageHeight)
-        if (newY > 0) {
+        const percent = Math.abs(newVal / this.imageHeight)
+        if (newVal > 0) {
           scale = 1 + percent
           zIndex = 10
         } else {
-          blur = Math.min(20 * percent, 20)
+          blur = Math.min(20, percent * 20)
         }
-        console.log(this.$refs.filter.style)
+
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
         this.$refs.filter.style[backdrop] = `blur(${blur}px)`
-        if (newY < this.minTranslateY) {
+        if (newVal < this.minTransalteY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
@@ -101,16 +117,16 @@
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
-          this.$refs.playBtn.style.display = 'block'
+          this.$refs.playBtn.style.display = ''
         }
-        this.$refs.bgImage.style.zIndex = zIndex
         this.$refs.bgImage.style[transform] = `scale(${scale})`
+        this.$refs.bgImage.style.zIndex = zIndex
       }
     },
     components: {
       Scroll,
-      SongList,
-      Loading
+      Loading,
+      SongList
     }
   }
 </script>
